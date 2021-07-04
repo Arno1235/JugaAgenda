b1d782a365330a53ea2dcef062c950e973b3bd4c
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-07-04 15:58:14 +0200

    Update Form1.cs

--- JugaAgenda/Form1.cs
+++ JugaAgenda/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace JugaAgenda
 {
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,19 +18,127 @@ namespace JugaAgenda
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void butWerkToevoegen_Click(object sender, EventArgs e)
         {
+            if (checkWerkGegevens())
+            {
+                Werk test = new Werk(textWerkBeschrijving.Text, numWerkUren.Value, checkWerkPrioriteit.Checked);
+                
+                if (checkWerkPrioriteit.Checked)
+                {
+                    //TODO: insert after other prioriteit elements
+                    listWerk.Items.Insert(0, test);
+                } else
+                {
+                    listWerk.Items.Add(test);
+                }
+                
 
+                textWerkBeschrijving.Text = "";
+                numWerkUren.Value = 0;
+                checkWerkPrioriteit.Checked = false;
+            }
+            
         }
 
-        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        private bool checkWerkGegevens()
         {
-            MessageBox.Show("Gemaakt door Arno ;)");
+            if (textWerkBeschrijving.Text.Equals(""))
+            {
+                MessageBox.Show("Vul een beschrijving in.", "Error");
+            }
+            else if (numWerkUren.Value == 0)
+            {
+                MessageBox.Show("Het aantal uren moet groter zijn dan 0.", "Error");
+            }
+            else
+            {
+                return true;
+            }
+            return false;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void menuUpdate_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Add("beh");
+            updateAgenda();
+        }
+
+        private void updateAgenda()
+        {
+            int currentDay = 0;
+            List <ListBox> listBoxList = new List<ListBox>() { listMaandag, listDinsdag, listWoensdag, listDonderdag, listVrijdag, listZaterdag, listZondag };
+            List<NumericUpDown> numericList = new List<NumericUpDown>() { numMaandagUren, numDinsdagUren, numWoensdagUren, numDonderdagUren, numVrijdagUren, numZaterdagUren, numZondagUren };
+            decimal overtime = 0;
+
+            foreach (Werk werk in listWerk.Items)
+            {
+                if (currentDay < listBoxList.Count())
+                {
+                    decimal timeToFillIn = numericList[currentDay].Value;
+                    foreach (Werk dagWerk in listBoxList[currentDay].Items)
+                    {
+                        timeToFillIn -= dagWerk.getUren();
+                    }
+                    if (werk.getUren() < timeToFillIn)
+                    {
+                        listBoxList[currentDay].Items.Add(werk);
+                    } else if (werk.getUren() == timeToFillIn)
+                    {
+                        listBoxList[currentDay].Items.Add(werk);
+                        currentDay++;
+                    } else
+                    {
+                        listBoxList[currentDay].Items.Add(new Werk(werk.getBeschrijving(), timeToFillIn, werk.getPrioriteit()));
+                        overtime = werk.getUren() - timeToFillIn;
+                        currentDay++;
+                    }
+                }
+            }
+        }
+
+        private void menuInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gemaakt door Arno ;)", "Info");
+        }
+        
+    }
+    public class Werk
+    {
+
+        String beschrijving;
+        decimal uren;
+        bool prioriteit;
+
+        public Werk(String beschrijving, decimal uren, bool prioriteit)
+        {
+            this.beschrijving = beschrijving;
+            this.uren = uren;
+            this.prioriteit = prioriteit;
+        }
+
+        public string getBeschrijving()
+        {
+            return this.beschrijving;
+        }
+        public decimal getUren()
+        {
+            return this.uren;
+        }
+        public bool getPrioriteit()
+        {
+            return this.prioriteit;
+        }
+
+        public override string ToString()
+        {
+            String returnString = this.uren.ToString() + "u - " + this.beschrijving;
+            if (this.prioriteit)
+            {
+                returnString += " - Prioriteit";
+            }
+            return  returnString;
         }
     }
+
 }
+
