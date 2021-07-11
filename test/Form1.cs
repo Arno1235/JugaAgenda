8798d0b09797c042957e442537fac558fbef0fd7
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-07-11 12:05:33 +0200

    Update Form1.cs

--- test/Form1.cs
+++ test/Form1.cs
@@ -17,9 +17,41 @@ namespace test
             InitializeComponent();
         }
 
+        private void monthView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (monthView1.SelectionStart < calendar1.ViewStart)
+            {
+                calendar1.ViewStart = monthView1.SelectionStart;
+                calendar1.ViewEnd = monthView1.SelectionEnd;
+            } else
+            {
+                calendar1.ViewEnd = monthView1.SelectionEnd;
+                calendar1.ViewStart = monthView1.SelectionStart;
+            }
+            
+        }
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Manual");
+            comboBox1.Items.Add("Day");
+            comboBox1.Items.Add("Week");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            monthView1.SelectionMode = System.Windows.Forms.Calendar.MonthView.MonthViewSelection.Manual;
+        }
+
+        private void calendar1_LoadItems(object sender, System.Windows.Forms.Calendar.CalendarLoadEventArgs e)
         {
 
         }
+
+        private void calendar1_CreateItem(object sender, System.Windows.Forms.Calendar.CalendarItemCancelEventArgs e)
+        {
+            MessageBox.Show(e.Item.Text.ToString());
+        }
+
     }
 }
