6a78a80354a2d6ac59c36354f550dcf4a31db735
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-07-11 18:09:59 +0200

    Update Program.cs

--- JugaAgenda_v2/Program.cs
+++ JugaAgenda_v2/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Text;
+using System.Threading;
+
 namespace JugaAgenda_v2
 {
     static class Program
@@ -14,6 +23,24 @@ namespace JugaAgenda_v2
         [STAThread]
         static void Main()
         {
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
