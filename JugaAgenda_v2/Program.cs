add2de447fe2ecd9129bdab8d3e5f408d11fdbd1
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-07-11 17:36:36 +0200

    Update Program.cs

--- JugaAgenda_v2/Program.cs
+++ JugaAgenda_v2/Program.cs
@@ -17,7 +17,7 @@ namespace JugaAgenda_v2
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new fHome());
         }
     }
 }
