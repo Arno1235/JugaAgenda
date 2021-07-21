a0f7296ff641b51db0d4ca6ae7fc9bb67aff5c2f
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-07-21 17:04:16 +0200

    Update fHome.cs

--- JugaAgenda_v2/fHome.cs
+++ JugaAgenda_v2/fHome.cs
@@ -8,14 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Calendar.v3;
-using Google.Apis.Calendar.v3.Data;
-using Google.Apis.Services;
-using Google.Apis.Util.Store;
-using System.IO;
-using System.Threading;
-
 namespace JugaAgenda_v2
 {
 
@@ -74,7 +66,7 @@ namespace JugaAgenda_v2
                 }
                 calHome.MaximumViewDays = oldMaximumViewDays;
 
-                foreach (var eventItem in googleCalendar.events.Items)
+                foreach (var eventItem in googleCalendar.getEvents().Items)
                 {
                     if (eventItem.Start.DateTime != null)
                     {
@@ -191,59 +183,5 @@ namespace JugaAgenda_v2
             return dt.AddDays(diff).Date;
         }
     }
-
-    public class GoogleCalendar
-    {
-        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
-        static string ApplicationName = "JugaAgenda";
-
-        private CalendarService service;
-
-        public Events events;
-
-        public GoogleCalendar()
-        {
-            UserCredential credential;
-
-            using (var stream =
-                new FileStream("client_secret_517386861162-oml2v6ifqe37dbsh4ls2u023pp89c9de.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
-            {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
-
-            // Create Google Calendar API service.
-            service = new CalendarService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-        }
-
-        public void refreshEvents()
-        {
-            // Define parameters of request.
-            EventsResource.ListRequest request = service.Events.List("pvdr3fefd859hoau6aop4jn9p8@group.calendar.google.com");
-            request.TimeMin = DateTime.Now;
-            request.ShowDeleted = false;
-            request.SingleEvents = true;
-            request.MaxResults = 10;
-            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
-
-            // List events.
-            events = request.Execute();
-        }
-
-    }
-
-    
 
 }
