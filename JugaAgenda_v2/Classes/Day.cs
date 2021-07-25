97a9f209334757c1167d4c7b42c0d8398700f8c3
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-07-25 11:33:18 +0200

    Create Day.cs

---
+++ JugaAgenda_v2/Classes/Day.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace JugaAgenda_v2.Classes
+{
+    class Day
+    {
+        private DateTime date;
+        private ArrayList workList;
+
+        public Day(DateTime date)
+        {
+            this.date = date;
+        }
+
+        #region getters
+        public ArrayList getWorkList()
+        {
+            return workList;
+        }
+        #endregion
+
+        #region setters
+        public void addWorkList(Work work)
+        {
+            workList.Add(work);
+        }
+        #endregion
+    }
+}
