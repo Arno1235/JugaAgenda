c947552196988fb2954bcaa2e6290c283e9aabcc
Arno Van Eetvelde <arno.ve2000@gmail.com>
2021-07-25 12:04:28 +0200

    Create Technician.cs

---
+++ JugaAgenda_v2/Classes/Technician.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JugaAgenda_v2.Classes
+{
+    class Technician
+    {
+    }
+}
